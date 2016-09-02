c2dbc28e1f44332079dcb3ad1e8d0b58514671f3
Nikolai Botev <nikolai_botev@yahoo.com>
2016-09-02 13:56:18 -0700

    Updated docker-compose to use nvm

--- docker-run.sh
+++ docker-run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 exec docker run --rm \
-  -v $(pwd):/project \
-  -w /project \
+  -v $(pwd):/code \
+  -w /code \
   --tty \
   centos:7 \
-  bash -lc 'cd && curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.6/install.sh | bash && . ~/.nvm/nvm.sh && cd - && nvm install && npm install && exec npm run webpack'
+  bash -lc 'curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.6/install.sh | bash && cd && . ~/.nvm/nvm.sh && cd - && nvm install && npm install && exec npm run webpack'
